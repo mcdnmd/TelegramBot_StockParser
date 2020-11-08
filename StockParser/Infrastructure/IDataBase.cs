96bf806eee02e16eb62d14626fb233174f1afb6b
chkaff <yutyomchik@gmail.com>
2020-11-08 20:31:25 +0500

    Create and setting IDataBase.cs

---
+++ StockParser/Infrastructure/IDataBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace StockParser.Infrastructure
+{
+    public interface IDataBase
+    {
+        string host { get; }
+        string userName { get; }
+        string password { get; }
+        void InitializationСonnection();
+        void CloeConnection();
+        void AddUser(string userId);
+        void RemoveUser(string userId);
+        void SelectUser(string userId);
+        Dictionary<string, string> GetUserInfo(string userId);
+        List<Dictionary<string, string>> GetUsersWhere(); //Здесь будут перечислены все столбцы базы и они будут иметь стандартное значение
+                                                            //Например ( id = ""; token = "" и тд)
+    }
+}
