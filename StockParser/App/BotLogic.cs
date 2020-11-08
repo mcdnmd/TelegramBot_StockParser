d5ffa9d39e054e4c81837fcbce8dbc6bb37a5efc
kir <kpoltoradnev@gmail.com>
2020-11-08 21:13:26 +0500

    #1.3 some changes done

--- StockParser/App/BotLogic.cs
+++ StockParser/App/BotLogic.cs
@@ -1,3 +1,4 @@
+using System;
 using StockParser.App.BotTask;
 
 namespace StockParser.App
@@ -6,7 +7,20 @@ namespace StockParser.App
     {
         public static void ExecuteTask(IBotTask task)
         {
-            
+            var userId = task.UserApi;
+            var type = task.Type;
+            switch (type)
+            {
+                case BotTask.BotTask.AddSymbol:
+                    break;
+                case BotTask.BotTask.RemoveSymbol:
+                    break;
+                case BotTask.BotTask.GetSymbolInfo:
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
